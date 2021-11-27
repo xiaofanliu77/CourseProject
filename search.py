90d51499bcfffcf52b2adf4e708264a72b04a2fd
brittany.west@biofiredx.com <brittany.west@biofiredx.com>
2021-11-27 12:45:47 -0700

    Data Cleaning

--- search.py
+++ search.py
@@ -3,6 +3,9 @@ import sys
 import time
 import metapy
 import pytoml
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 
 if __name__ == '__main__':
