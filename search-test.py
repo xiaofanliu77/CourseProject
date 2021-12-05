75cb94e55192c5e3c026e19b958a9e8c06d0e3f1
bpang97 <bpang97>
2021-12-05 14:40:39 -0800

    fix print highlighting and add user input

--- search-test.py
+++ search-test.py
@@ -11,11 +11,45 @@ import re
 # sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 # sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
-def print_with_highlight(content, query, doc_id=None):
+stopwords = []
+
+'''
+Prints a truncated version of doc containing the first k occurences
+of the words in query, where the k words in query are highlighted
+'''
+def print_with_highlight(doc, query, k, doc_id=None):
     highlight_list = []
     for w in query.split(" "):
-        highlight_list.append(w.capitalize())
-        highlight_list.append(w.lower())
+        if w not in stopwords:
+            highlight_list.append(w.capitalize())
+            highlight_list.append(w.lower())
+            highlight_list.append(w.upper())
+
+
+    words = doc.split(" ")
+    count = 0
+    start, end = -1, -1
+    seen = False
+    for i, word in enumerate(words):
+        if word in highlight_list and len(word) >= 1:
+            if not seen:
+                start = i
+                seen = True
+                print("seen: ", word, i)
+            count += 1
+            if count == k:
+                end = i
+                break
+
+    if start == -1 or end == -1:
+        cutoff = 150
+        content = ' '.join(words[:cutoff]) + "......"
+    else:
+        print("start, end", start, end)
+        i = max(start - 4, 0)
+        j = min(end + 4, len(words))
+        content_words = ' '.join(words[i:j])
+        content = "......" + content_words + "......"
 
 
     highlight_str = r"\b(?:" + '|'.join(highlight_list) + r")\b"
@@ -27,15 +61,13 @@ def print_with_highlight(content, query, doc_id=None):
 
 if __name__ == '__main__':
 
+    with open('stopwords.txt') as f:
+        stopwords = f.read().splitlines()
+
     cfg = "config.toml"
-    # print('Building or loading index...')
     idx = metapy.index.make_inverted_index(cfg)
     ranker = metapy.index.OkapiBM25()
-    # ranker = metapy.index.OkapiBM25(k1=1.2,b=0.75,k3=0)
-    # ranker = metapy.index.JelinekMercer()
 
-
-    # ev = metapy.index.IREval(cfg)
 
     with open(cfg, 'r') as fin:
         cfg_d = pytoml.load(fin)
@@ -45,27 +77,47 @@ if __name__ == '__main__':
         print("query-runner table needed in {}".format(cfg))
         sys.exit(1)
 
-    # start_time = time.time()
+
+
     top_k = 10
     query_path = query_cfg.get('query-path', 'CampusWireHeading_Queries.txt')
     query_start = query_cfg.get('query-id-start', 0)
 
     query = metapy.index.Document()
-    query_text = "Question on Probabilistic retrieval model"
+    query_text = input("please enter a search query: ")
+    ranking_function = input("""Please select a ranking function. Enter
+        1 for Okapi BM25
+        2 for Jelinek-Mercer
+        3 for DirichletPrior
+""")
 
     query.content(query_text.lower())
 
     top_docs = ranker.score(idx, query, num_results=10)
-    print(top_docs)
 
+    print("top docs: ")
+    print(top_docs)
     print(idx.num_docs())
 
     for num, (d_id, _) in enumerate(top_docs):
         content = idx.metadata(d_id).get('content')
         if content is not None:
-            print_with_highlight(content, query_text, d_id)
+            print_with_highlight(content, query_text, 5, d_id)
 
-            # print("{}. {}...\n".format(d_id, text_highlight))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     # # ndcg = 0.0
